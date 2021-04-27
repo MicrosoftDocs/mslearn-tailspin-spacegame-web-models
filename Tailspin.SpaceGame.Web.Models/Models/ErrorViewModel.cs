074dc813b66da4161260f34a07d3bcd0cf63d99e
Bohdan Chornii <bchornii@softserveinc.com>
2021-04-27 22:28:28 +0300

    update versioning

--- Tailspin.SpaceGame.Web.Models/Models/ErrorViewModel.cs
+++ Tailspin.SpaceGame.Web.Models/Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace TailSpin.SpaceGame.Web.Models
 {
     public class ErrorViewModel
     {
+        // Request Id assigned by error handling code.
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
