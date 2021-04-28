0b78725c604b6c354a3955ce8483b3dc2d312f11
Bohdan Chornii <bchornii@softserveinc.com>
2021-04-28 20:40:59 +0300

    removed comments 3

--- Tailspin.SpaceGame.Web.Models/Models/LeaderboardViewModel.cs
+++ Tailspin.SpaceGame.Web.Models/Models/LeaderboardViewModel.cs
@@ -9,10 +9,8 @@ namespace TailSpin.SpaceGame.Web.Models
 
         // The game region (map) selected in the view.
         public string SelectedRegion { get; set; }
-
         // The current page to be shown in the view.
         public int Page { get; set; }
-
         // The number of items to show per page in the view.
         public int PageSize { get; set; }
         // The scores to display in the view.
