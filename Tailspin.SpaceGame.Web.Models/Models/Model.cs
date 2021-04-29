e987305e4279a0b8ad441c0c654d40388b0513dd
bchornii <bogdan.chorniy@gmail.com>
2021-04-29 10:55:15 +0300

    Code style fixes +semver: patch
    Co-authored-by: Bohdan Chornii <bchornii@softserveinc.com>

--- Tailspin.SpaceGame.Web.Models/Models/Model.cs
+++ Tailspin.SpaceGame.Web.Models/Models/Model.cs
@@ -10,6 +10,7 @@ namespace TailSpin.SpaceGame.Web.Models
         // The value that uniquely identifies this object.
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
+        
         // The value that uniquely identifies this object.
         [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
